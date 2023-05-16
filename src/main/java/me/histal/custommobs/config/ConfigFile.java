835e26ca6bd2718f96e50b276e86898d6320f538
Vojtěch Riedl <vojtariedl66@gmail.com>
2023-05-16 14:40:20 +0200

    add user management

---
+++ src/main/java/me/histal/custommobs/config/ConfigFile.java
@@ -0,0 +1,87 @@
+package me.histal.custommobs.config;
+
+import me.histal.custommobs.CustomMobs;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigFile {
+
+    File file;
+    FileConfiguration configuration;
+
+    CustomMobs plugin;
+    String name;
+    String folder;
+
+    public ConfigFile(String name, boolean createDefault) {
+        this("", name,createDefault);
+    }
+
+    public ConfigFile(String folder, String name, boolean createDefault) {
+        this(CustomMobs.getInstance(), folder, name, createDefault);
+    }
+
+    public ConfigFile(CustomMobs plugin,String folder, String name,boolean createDefault) {
+        this.plugin = plugin;
+        this.folder = folder;
+        this.name = name;
+        this.configuration = YamlConfiguration.loadConfiguration(this.file = new File(plugin.getDataFolder() + this.folder, this.name + ".yml"));
+        if(createDefault) {
+            createNewFile();
+        }
+    }
+
+    public boolean exists() {
+        return this.file.exists();
+    }
+
+    public void createNewFile(){
+        if(!this.exists()) {
+            try{
+                this.file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void reload() {
+        this.configuration = YamlConfiguration.loadConfiguration(this.file);
+    }
+
+    public void save() {
+        try {
+            this.configuration.save(this.file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void asyncSave() {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, this::save);
+    }
+
+    public void asyncDelete() {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, this::removeFile);
+    }
+
+
+    public FileConfiguration getConfig(){
+        return this.configuration;
+    }
+
+    public File getFile(){
+        return this.file;
+    }
+
+    public void removeFile() {
+        if(exists()){
+            this.file.delete();
+        }
+    }
+
+}
---
+++ src/main/java/me/histal/custommobs/user/User.java
@@ -0,0 +1,34 @@
+package me.histal.custommobs.user;
+
+import org.bukkit.entity.Player;
+
+public class User {
+
+    Player player;
+
+    int killedMobs;
+
+    public User(int killedMobs) {
+        this.killedMobs = killedMobs;
+    }
+
+    public User(){
+        this(0);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public int getKilledMobs() {
+        return killedMobs;
+    }
+
+    public void setKilledMobs(int killedMobs) {
+        this.killedMobs = killedMobs;
+    }
+}
