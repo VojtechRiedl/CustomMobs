7ee6442d9fa063de503761e3d2fdd7244903cff5
Vojtěch Riedl <vojtariedl66@gmail.com>
2023-05-16 14:40:20 +0200

    add user management

---
+++ src/main/java/me/histal/custommobs/user/UserManager.java
@@ -0,0 +1,52 @@
+package me.histal.custommobs.user;
+
+import me.histal.custommobs.CustomMobs;
+
+import java.util.HashMap;
+
+public class UserManager {
+
+    private CustomMobs plugin;
+
+    HashMap<String, User> users;
+
+    public UserManager(){
+        this.plugin = CustomMobs.getInstance();
+        loadData();
+    }
+
+    public void loadData(){
+        users = new HashMap<>();
+    }
+
+    public User createOrGetUser(String name) {
+        if(name == null || name.isEmpty()){
+            return null;
+        }
+        User user = getUser(name);
+        if(user != null){
+            return user;
+        }
+        user = new User();
+        users.put(name.toLowerCase(), user);
+        return user;
+    }
+
+
+    public User getUser(String name) {
+        if(name == null || name.isEmpty()){
+            return null;
+        }
+        if(!existUser(name)){
+            return null;
+        }
+
+        return users.get(name);
+    }
+
+    public boolean existUser(String name) {
+        return users.containsKey(name);
+    }
+
+
+}
